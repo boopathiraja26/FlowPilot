e275d727b7a3a4a9278c3ed1101fde03cbff9bf8
boopathiraja26 <boopathigamingff@gmail@gmail.com>
2026-09-07 11:23:18 +0530

    fix: stabilize Next.js production deployment

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
+  outputFileTracingRoot: path.join(__dirname, "../"),
   eslint: {
     dirs: ["src"],
   },
